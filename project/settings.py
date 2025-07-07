eaa2ca1b351378edd4a75a05910f4153d9aa98d3
Shauryaman Sawai <sawaisushil@gmail.com>
2025-07-07 20:20:15 +0530

    vercel_ready_1

--- project/settings.py
+++ project/settings.py
@@ -148,7 +148,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# settings.py
+STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles_build/static'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
