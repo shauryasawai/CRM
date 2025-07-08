cfe03907697a449db28cc11d7e4b2ba58f8d439c
Shauryaman Sawai <sawaisushil@gmail.com>
2025-07-08 21:59:06 +0530

    attempt_7

--- project/settings.py
+++ project/settings.py
@@ -64,7 +64,7 @@ TIME_ZONE = 'Asia/Kolkata'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
